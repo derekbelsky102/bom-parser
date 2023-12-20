9d3b779ffc782d3821f3b3f7909c3cc8c13551eb
Derek Belsky <dbelsky@srcinc.com>
2023-12-19 22:38:36 -0500

    Added exception and changed json parsing to something that handles single quotes

--- bom_parser.py
+++ bom_parser.py
@@ -3,6 +3,7 @@ import os
 from pathlib import Path
 import re
 import platform
+import ast
 
 import digikey
 from digikey.v3.productinformation import KeywordSearchRequest
@@ -23,48 +24,12 @@ def digikey_manuf_search(x):
     result = digikey.keyword_search(body=search_request)
     return str(result)
 
-# Json File needs some parsing before it is ready to parse through
-def format_json_file(string):
-    formatted_result = string.replace('"', '\\"')
-    formatted_result = formatted_result.replace('\'', '"')
-    
-    # Specific Keywords that aren't in quotes that should be
-    formatted_result = formatted_result.replace('None', '"None"')
-    formatted_result = formatted_result.replace('False', '"False"')
-    formatted_result = formatted_result.replace('True', '"True"')
-    
-    # \\" should only mean inches at this point
-    formatted_result = formatted_result.replace('\\"', ' in')
-
-    space_form_result =  []
-    combined_line = ''
-    for line in formatted_result.splitlines():
-        if(line[-1] == ','):
-            if(combined_line != ''):
-                combined_line = combined_line + line.strip().replace('"', '').replace(',', '') + '",' 
-                space_form_result.append(combined_line+"\n")
-                #print(combined_line)
-                combined_line = ''
-            else:
-                space_form_result.append(line+"\n")
-        else:
-            if(combined_line == ''):
-                combined_line = line[:-1] #need to remove last quote
-            else:
-                combined_line = combined_line + line.strip().replace('"', '')
-    space_form_result.append(line+"\n")
-            
-    return ''.join(space_form_result)
-          
-
 class Part_Number:
     def __init__(self, part_number):
         pre_format_json = digikey_manuf_search(part_number)
-        formatted_json = format_json_file(pre_format_json)
-
-        part_num_info = json.loads(formatted_json)
         
         self.part_number = ""
+        self.part_type = ""
         self.description = ""
         self.manufacturer = ""
         self.value = ""
@@ -76,46 +41,50 @@ class Part_Number:
         self.error = ""
         self.operating_temp = ""
         
-        #  Digikey routes to alternatives sometimes. Check for alternatives
-        if part_num_info["exact_manufacturer_products"]:
-            part_num_product_type = part_num_info["exact_manufacturer_products"][0]
-        else:
-            part_num_product_type = part_num_info["products"][0]
-        
-        self.part_number = part_num_product_type["manufacturer_part_number"]
-        self.description = part_num_product_type["product_description"]
-        self.manufacturer = part_num_product_type["manufacturer"]['value']
-        self.part_type = part_num_product_type["category"]["value"]
-        
-        parameters =  part_num_product_type["parameters"]
-        # Grab general parameters
-        for line in parameters:
-            if line["parameter"] == "Operating Temperature":
-                self.operating_temp = line["value"]
-            if line["parameter"] == "Package / Case":
-                self.package = line["value"]
-        # Specific Parameters
-        if self.part_type == 'Resistors':
-            for line in parameters:
-                if line["parameter"] == "Resistance":
-                    self.value = line["value"]
-                if line["parameter"] == "Tolerance":
-                    self.tolerance = line["value"]
-                if line["parameter"] == "Power (Watts)":
-                    self.power = line["value"]
-        if self.part_type == 'Capacitors':
-            for line in parameters:
-                if line["parameter"] == "Capacitance":
-                    self.value = line["value"]
-                if line["parameter"] == "Tolerance":
-                    self.tolerance = line["value"]
-                if line["parameter"] == "Temperature Coefficient":
-                    self.dieletric = line["value"]
-                if line["parameter"] == "Voltage - Rated":
-                    self.voltage_rate = line["value"]
-            
-            
-    
+        try:
+            part_num_info = ast.literal_eval(pre_format_json)
+            try:
+                #  Digikey routes to alternatives sometimes. Check for alternatives
+                if part_num_info["exact_manufacturer_products"]:
+                    part_num_product_type = part_num_info["exact_manufacturer_products"][0]
+                else:
+                    part_num_product_type = part_num_info["products"][0]
+                
+                self.part_number = part_num_product_type["manufacturer_part_number"]
+                self.description = part_num_product_type["product_description"]
+                self.manufacturer = part_num_product_type["manufacturer"]['value']
+                self.part_type = part_num_product_type["category"]["value"]
+                
+                parameters =  part_num_product_type["parameters"]
+                # Grab general parameters
+                for line in parameters:
+                    if line["parameter"] == "Operating Temperature":
+                        self.operating_temp = line["value"]
+                    if line["parameter"] == "Package / Case":
+                        self.package = line["value"]
+                # Specific Parameters
+                if self.part_type == 'Resistors':
+                    for line in parameters:
+                        if line["parameter"] == "Resistance":
+                            self.value = line["value"]
+                        if line["parameter"] == "Tolerance":
+                            self.tolerance = line["value"]
+                        if line["parameter"] == "Power (Watts)":
+                            self.power = line["value"]
+                if self.part_type == 'Capacitors':
+                    for line in parameters:
+                        if line["parameter"] == "Capacitance":
+                            self.value = line["value"]
+                        if line["parameter"] == "Tolerance":
+                            self.tolerance = line["value"]
+                        if line["parameter"] == "Temperature Coefficient":
+                            self.dieletric = line["value"]
+                        if line["parameter"] == "Voltage - Rated":
+                            self.voltage_rate = line["value"]
+            except:
+                self.error = "No Parameters Returned"
+        except:
+            self.error = "Formatting Issue in Digikey Data"
 
 
 
--- runner.py
+++ runner.py
@@ -1,17 +1,46 @@
 from bom_parser import Part_Number
 from pprint import pprint
 import time
+import pandas as pd
+import csv
+import openpyxl
 
-bom_file = open('example_bom.txt', 'r')
-lines = bom_file.readlines()
+def export_csv(input_file, output_file):
 
-for line in lines:
-    part_num = line.strip('\n')
-    p = Part_Number(part_num)
-    if p.error == '':
-        print("{0} : {1}".format(part_num, p.operating_temp))
-    else:
-        print("{0} : {1}".format(part_num, p.error))
-#pprint(vars(p))
+    bom_file = open(input_file, 'r')
+    lines = bom_file.readlines()  
+        
+    # field names 
+    top_fields = ['General Part Attributes', '', '', '', '', '', '', '', \
+    'Passives Specific', '', 'Resistor Specific', 'Capacitor Specific', '']
+    desc_fields = ['Part Name Requested', 'Part Number Returned', 'Error', 'Part Type', \
+    'Description', 'Manufacturer', 'Operating Temp', 'Package', 'Value', 'Tolerance', 'Power', 'Voltage Rating', 'Dieletric'] 
+
+    # writing to csv file  
+    with open(output_file, 'w', newline='', encoding='utf-8') as csvfile:  
+        # creating a csv writer object  
+        csvwriter = csv.writer(csvfile)  
+            
+        # writing the fields  
+        csvwriter.writerow(top_fields)
+        csvwriter.writerow(desc_fields)
+
+        for line in lines:
+            part_num = line.strip('\n')
+            p = Part_Number(part_num)
+            print("Processing ", part_num)
+            list = [part_num, p.part_number, p.error, p.part_type, \
+            p.description, p.manufacturer, p.operating_temp, p.package, p.value, p.tolerance, \
+            p.power, p.voltage_rate, p.dieletric]
+            csvwriter.writerow(list)
+            
+input_file = 'example_bom.txt'
+output_file = 'example_output.csv'
+output_excel_file = 'output_bom.xlsx'
+export_csv(input_file, output_file)
+
+# Convert to xlsx for conditional formatting
+df = pd.read_csv(output_file, encoding='utf-8')
+df.to_excel(output_excel_file, index=None, header=True)
 
 
